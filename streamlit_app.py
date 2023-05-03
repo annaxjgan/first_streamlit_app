a0975a02dd457f97e7d2f5e677ca9bc21b1aefed
Anna Gan <125049826+annaxjgan@users.noreply.github.com>
2023-05-03 14:10:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,5 +31,5 @@ streamlit.text(fruityvice_response.json())
 # Flattening data into a flat table
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Create pandas table from data
-#streamlit.dataframe(fruityvice_normalized)
-fruityvice_normalized
+streamlit.dataframe(fruityvice_normalized)
+
